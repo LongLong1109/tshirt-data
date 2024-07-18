9e5416e17ec54db6bd42b99dcdfd9af3cad5231e
Long Viet Tran <long.tran@asnet.com.vn>
2024-07-18 17:49:33 +0700

    deploy data

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("server.json");
+const auth = require("json-server-auth");
+
+server.db = router.db;
+
+server.use((req, res, next) => {
+  const corsWhitelist = ["http://localhost:3000", "http://localhost:3001"];
+
+  if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    );
+  }
+
+  next();
+});
+
+server.use(auth);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port);
